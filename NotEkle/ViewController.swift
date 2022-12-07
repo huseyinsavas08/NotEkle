a79c36c246aa45b978cf8a72f552174d3808f4a2
Hüseyin Savaş <savas.08@hotmail.com>
2022-12-07 21:01:17 +0300

    Initial Commit

---
+++ NotEkle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotEkle
+//
+//  Created by Hüseyin Savaş on 7.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
